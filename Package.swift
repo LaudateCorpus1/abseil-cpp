8ddf129163673642a339d7980327bcb2c117a28e
Paul Beusterien <paulbeusterien@google.com>
2020-09-13 10:41:53 -0700

    Fix platform build warning

--- Package.swift
+++ Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
   name: "abseil",
-    platforms: [
-        .macOS(.v10_10),
-        .iOS(.v8),
-        .tvOS(.v9)
-    ],
-
   products: [
     .library(
       name: "abseil",
